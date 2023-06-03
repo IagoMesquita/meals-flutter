84c958dbab004ba1afbdaff0631d379649c86180
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-03 11:37:57 -0300

    feat: CategoryItem in categoriesScreen

--- lib/screen/categories_screen.dart
+++ lib/screen/categories_screen.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/category_item.dart';
+import '../data/dummy_data.dart';
 
-class CategoriesScrenn extends StatelessWidget {
-  const CategoriesScrenn({super.key});
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  final categories = DUMMY_CATEGORIES;
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const  SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Vamos Cozinhar?')),
+      ),
+      body: GridView(
+        gridDelegate: const  SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20
+        ),
+        children:  DUMMY_CATEGORIES.map((category) {
+          return CategoryItem(category: category,);
+        }).toList()
       ),
     );
   }
