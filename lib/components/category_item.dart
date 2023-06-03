e0d0592bb2d752d4d4b3f765a755f972d7126e32
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-03 11:58:19 -0300

    style: BoxDecoraion in CategoryItem

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -11,6 +11,20 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(child: Text(category.title),);
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          colors: [
+            category.color,
+            category.color.withOpacity(0.5),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight
+        ),
+      ),
+      child: Text(category.title),
+    );
   }
 }
