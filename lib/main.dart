8736874a26b3edf3ae78eb7c90e2e7d374577d8a
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-03 14:50:13 -0300

    feat: ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vamos Cozinhar?',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const CategoriesScreen()
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        fontFamily: 'Raleway',
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          titleLarge: const TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+          )
+        )
+
+      ),
+      home: const CategoriesScreen(),
     );
   }
 }
-
