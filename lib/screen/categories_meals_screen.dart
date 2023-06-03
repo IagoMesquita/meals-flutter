2eea2eadb9b29344ab383aabdca316b54471d788
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-03 19:29:04 -0300

    feat: ModalRoute to receive data on screen

--- lib/screen/categories_meals_screen.dart
+++ lib/screen/categories_meals_screen.dart
@@ -2,16 +2,15 @@ import 'package:flutter/material.dart';
 import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-
-  final Category category;
-
-  const CategoriesMealsScreen({super.key, required this.category});
+  const CategoriesMealsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final category = ModalRoute.of(context)?.settings.arguments as Category;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(category.title),
+        title: Text(category.id),
       ),
       body: Center(child: Text('RECEITAS POR CATEGORIA ${category.id}')),
     );
