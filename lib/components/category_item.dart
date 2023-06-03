5281129cd0e7fecdb8cb3cff86058d240fe161c6
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-03 14:52:16 -0300

    style: CategotyItem

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -12,19 +12,17 @@ class CategoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(colors: [
             category.color,
             category.color.withOpacity(0.5),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
         ),
-      ),
-      child: Text(category.title),
-    );
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ));
   }
 }
