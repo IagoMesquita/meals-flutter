876bc7a786268469c0991738cab9acf1c1626596
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-03 11:38:17 -0300

    feat: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screen/categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,26 +14,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Vamos Cozinhar?',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(),
+      home: const CategoriesScreen()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text('Vamos cozinhas?')),
-      ),
-      body: const Center(child: Text('Navegar é preciso.')),
-    );
-  }
-}
