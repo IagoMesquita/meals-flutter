f5c7099c08c1d6065f56b457b82c87ee81b90536
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-05 20:45:24 -0300

    feat: navigate meals page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screen/categories_screen.dart';
 import 'package:meals/screen/categories_meals_screen.dart';
+import 'package:meals/screen/meal_detail_screen.dart';
 import 'package:meals/utils/app_routes.dart';
 
 void main() {
@@ -28,6 +29,7 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.HOME: (ctx) => const CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => const CategoriesMealsScreen(),
+        AppRoutes.MEALS_DETAILS: (ctx) => const MealDetailScreen()
       },
     );
   }
--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,5 +1,6 @@
 class AppRoutes {
   static const HOME = '/';
   static const CATEGORIES_MEALS = '/categories-meals';
+  static const MEALS_DETAILS = '/meals-details';
 
 }
