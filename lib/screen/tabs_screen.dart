cc782ce9ab0845b794e5817959e286d623a63164
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-06 23:21:01 -0300

    feat: tabBarView

--- lib/screen/tabs_screen.dart
+++ lib/screen/tabs_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screen/categories_screen.dart';
+import 'package:meals/screen/fvorite_screen.dart';
 
 class TabsScreen extends StatelessWidget {
   const TabsScreen({super.key});
@@ -9,12 +11,16 @@ class TabsScreen extends StatelessWidget {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Vamos Cozinhar?'),
+          title: const Center(child:  Text('Vamos Cozinhar?')),
           bottom: const TabBar(tabs: [
            Tab(icon: Icon(Icons.category), child: Text('Categprias'),),
            Tab(icon: Icon(Icons.star), child: Text('Favoritos'),),
           ]),
         ),
+        body: const TabBarView(children: [
+          CategoriesScreen(),
+          FavoriteScreen(),
+        ]),
       ),
     );
   }
