aaeae62895a653b27ac3c57c0e367196f857c489
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-08 10:38:25 -0300

    feat: Settings Screen

---
+++ lib/screen/settings_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child:  Text('Configurações')),
+        // actions: [
+        //   IconButton(
+        //     onPressed: () {},
+        //     icon: const Icon(Icons.close)
+        //   )
+        // ],
+      ),
+      body: const Center(
+        child: Text('Configurações!!'),
+      ),
+    );
+  }
+}
