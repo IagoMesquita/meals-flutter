c4a6e709343151609e8ce8e3e7b772ceffa50c36
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-06-09 18:02:56 -0300

    feat: render componente favorite

---
+++ lib/screen/favorite_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
+import '../models/meal.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+
+  const FavoriteScreen({super.key, required this.favoriteMeals});
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return const Center(
+        child: Text('Nenhuma refeição foi retornada como favorita'),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: favoriteMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            meal: favoriteMeals[index],
+          );
+        },
+      );
+    }
+  }
+}
